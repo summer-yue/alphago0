0231c06e5f782683422d36673d9da2e2049cf23c
Summer Yue <yutingyue514@gmail.com>
2018-01-21 20:40:12 -0500

    integrate go_board with go_gui with no rules enforced

--- go_gui.py
+++ go_gui.py
@@ -1,7 +1,8 @@
 import pygame
+import go_board as gb
 from pygame.locals import *
 
-BOARD_SIZE = 9 # Define an x by x board
+BOARD_DIM = 9 # Define an x by x board
 
 # Define colors
 BLACK  = (0, 0, 0)
@@ -9,20 +10,23 @@ WHITE  = (245, 245, 245)
 RED    = (133, 42, 44)
 YELLOW = (208, 176, 144)
 GREEN  = (26, 81, 79)
-PLAYER = False
+
+PLAYER_BLACK = 1
+PLAYER_WHITE = -1
+EMPTY = 0
 
 # Define grid globals
 WIDTH = 20 # Width of each square on the board
 MARGIN = 1 # How thick the lines are
 PADDING = 20 # Distance between board and border of the window
 DOT = 4 # Number of dots
-BOARD = (WIDTH + MARGIN) * (BOARD_SIZE - 1) + MARGIN # Actual width for the board
+BOARD = (WIDTH + MARGIN) * (BOARD_DIM - 1) + MARGIN # Actual width for the board
 GAME_WIDTH = BOARD + PADDING * 2
 GAME_HIGHT = GAME_WIDTH + 100
 
 class Go:
     def __init__(self):
-        self.grid = [[0 for x in range(BOARD_SIZE)] for y in range(BOARD_SIZE)]
+        self.go_board = gb.go_board(board_dimension=BOARD_DIM, player=PLAYER_BLACK)
         pygame.init()
         pygame.font.init()
         self._display_surf = pygame.display.set_mode((GAME_WIDTH,GAME_HIGHT), pygame.HWSURFACE | pygame.DOUBLEBUF)
@@ -40,27 +44,27 @@ class Go:
 
         if event.type == pygame.MOUSEBUTTONUP:
             pos = pygame.mouse.get_pos()
-            global PLAYER
             if self.mouse_in_botton(pos):
                 if not self._playing:
                     self.start()
-                    if PLAYER:
-                        PLAYER = not PLAYER
                 else:
                     self.surrender()
-                    PLAYER = not PLAYER
+                    self.go_board.flip_player()
 
             elif self._playing:
                 r = (pos[0] - PADDING + WIDTH // 2) // (WIDTH + MARGIN)
                 c = (pos[1] - PADDING + WIDTH // 2) // (WIDTH + MARGIN)
 
-                if 0 <= r < BOARD_SIZE and 0 <= c < BOARD_SIZE:
-                    if self.grid[r][c] == 0:
+                if 0 <= r < BOARD_DIM and 0 <= c < BOARD_DIM:
+                    if self.go_board.board_grid[r][c] == 0:
                         self.lastPosition = [r,c]
-                        self.grid[r][c] = 1 if PLAYER else 2
+                        self.go_board.add_move_to_history(r, c)
+                        self.go_board.board_grid[r][c] = self.go_board.player
+                        self.go_board.flip_player()
 
-                        PLAYER = not PLAYER
-
+        print(self.go_board)
+        print()
+    
     def on_render(self):
         self.render_go_piece()
         self.render_last_position()
@@ -83,15 +87,12 @@ class Go:
 
     def start(self):
         self._playing = True
-        self.grid = [[0 for x in range(BOARD_SIZE)] for y in range(BOARD_SIZE)]
         self.lastPosition = [-1,-1]
         self._win = False
-
 
     def surrender(self):
         self._playing = False
         self._win = True
-
 
     def go_board_init(self):
         self._display_surf.fill(YELLOW)
@@ -103,8 +104,8 @@ class Go:
                           BOARD]) 
 
         # Draw the grid
-        for row in range(BOARD_SIZE - 1):
-            for column in range(BOARD_SIZE - 1):
+        for row in range(BOARD_DIM - 1):
+            for column in range(BOARD_DIM - 1):
                 pygame.draw.rect(self._display_surf, YELLOW,
                                  [(MARGIN + WIDTH) * column + MARGIN + PADDING,
                                   (MARGIN + WIDTH) * row + MARGIN + PADDING,
@@ -122,10 +123,11 @@ class Go:
 
 
     def mouse_in_botton(self,pos):
+        """ Check if mouse is in the button and return a boolean value
+        """
         if GAME_WIDTH // 2 - 50 <= pos[0] <= GAME_WIDTH // 2 + 50 and GAME_HIGHT - 50 <= pos[1] <= GAME_HIGHT - 20:
            return True
         return False
-
 
     def render_button(self):
         color = GREEN if not self._playing else RED
@@ -141,14 +143,12 @@ class Go:
         textRect.centery = GAME_HIGHT - 35
         self._display_surf.blit(text, textRect)
 
-
     def render_game_info(self):
         #current player color
-        color = BLACK if not PLAYER else WHITE
+        color = BLACK if self.go_board.player == PLAYER_BLACK else WHITE
         center = (GAME_WIDTH // 2 - 60, BOARD + 60)
         radius = 12
         pygame.draw.circle(self._display_surf, color, center, radius, 0)
-
 
         info = "You Win" if self._win else "Your Turn"
         info_font = pygame.font.SysFont('Helvetica', 24)
@@ -159,19 +159,23 @@ class Go:
         self._display_surf.blit(text, textRect)
 
     def render_go_piece(self):
-        for r in range(BOARD_SIZE):
-            for c in range(BOARD_SIZE):
+        """ Render the Go stones on the board according to self.go_board
+        """
+        for r in range(BOARD_DIM):
+            for c in range(BOARD_DIM):
                 center = ((MARGIN + WIDTH) * r + MARGIN + PADDING,
                           (MARGIN + WIDTH) * c + MARGIN + PADDING)
-                if self.grid[r][c] > 0:
-
-                    color = BLACK if self.grid[r][c] == 2 else WHITE
+                if self.go_board.board_grid[r][c] != EMPTY:
+                    color = BLACK if self.go_board.board_grid[r][c] == PLAYER_BLACK else WHITE
                     pygame.draw.circle(self._display_surf, color,
                                        center,
-                                       WIDTH // 2 - MARGIN,0)
+                                       WIDTH // 2 - MARGIN,
+                                       0)
 
 
     def render_last_position(self):
+        """ Render a red rectangle around the last position
+        """
         if self.lastPosition[0] > 0 and self.lastPosition[1] > 0:
             pygame.draw.rect(self._display_surf,RED,
                              ((MARGIN + WIDTH) * self.lastPosition[0] + (MARGIN + WIDTH) // 2, 
