c7758e855dede642e9ae0850b58391c04d96056c
Summer Yue <yutingyue514@gmail.com>
2018-02-22 14:04:40 -0500

    Fix reset relu layer bug

--- value_policy_net/resnet.py
+++ value_policy_net/resnet.py
@@ -21,14 +21,16 @@ class ResNet():
         self.go_board_dimension = go_board_dimension
 
         #Define the tensors that compose the graph
-        self.x = tf.placeholder(tf.float32, [None, self.go_board_dimension, self.go_board_dimension, 4], name="input")
+        self.x = tf.placeholder(tf.float32, [None, self.go_board_dimension, self.go_board_dimension, 3], name="input")
         self.yp = tf.placeholder(tf.float32, [None,  self.go_board_dimension*self.go_board_dimension + 1], name="labels_p")
         self.yv = tf.placeholder(tf.float32, [None, 1], name="labels_v")
         self.yp_, self.yv_, self.yp_logits, self.yv_logits = self.build_network(self.x) 
 
         self.calc_loss()
-        self.optimizer = tf.train.MomentumOptimizer(1e-3, 0.9)
-        #self.optimizer = tf.train.AdamOptimizer()
+        self.gradient = tf.gradients(self.loss, self.x)
+        self.calc_accuracy()
+        #self.optimizer = tf.train.MomentumOptimizer(1e-4, 0.9)
+        self.optimizer = tf.train.AdamOptimizer()
         self.train_op = self.optimizer.minimize(self.loss)
 
         self.sess = tf.get_default_session()
@@ -38,20 +40,48 @@ class ResNet():
             saver = tf.train.Saver(max_to_keep=500)
             saver.restore(self.sess, model_path)
 
+    def calc_accuracy(self):
+        """Calculate the accuracy function for the fake value network
+        used in fake_train testing
+        Returns:
+            The accuracy tensor
+        """
+        elements_0 = tf.equal(self.yv, 0)
+        elements_0 = tf.cast(elements_0, tf.int32)
+        predicted_right_count_zero = tf.less(tf.abs(self.yv - self.yv_), 0.3)
+        predicted_right_count_zero = tf.cast(predicted_right_count_zero, tf.int32)
+        predicted_right_count_zero = tf.multiply(elements_0, predicted_right_count_zero)
+        predicted_right_count_zero = tf.reduce_sum(predicted_right_count_zero)
+
+        elements_1 = tf.equal(self.yv, 1)
+        elements_1 = tf.cast(elements_1, tf.int32)
+        predicted_right_count_one = tf.less(tf.subtract(self.yv, self.yv_), 0.7)
+        predicted_right_count_one = tf.cast(predicted_right_count_one, tf.int32)
+        predicted_right_count_one = tf.multiply(elements_1, predicted_right_count_one)
+        predicted_right_count_one = tf.reduce_sum(predicted_right_count_one)
+
+        elements_neg1 = tf.equal(self.yv, -1)
+        elements_neg1 = tf.cast(elements_neg1, tf.int32)
+        predicted_right_count_neg_one = tf.less(tf.subtract(self.yv_, self.yv), 0.7)
+        predicted_right_count_neg_one = tf.cast(predicted_right_count_neg_one, tf.int32)
+        predicted_right_count_neg_one = tf.multiply(elements_neg1, predicted_right_count_neg_one)
+        predicted_right_count_neg_one = tf.reduce_sum(predicted_right_count_neg_one)
+        
+        self.accuracy = (predicted_right_count_zero + predicted_right_count_one + predicted_right_count_neg_one)/tf.size(elements_0)
+
     def calc_loss(self):
         """Calculate the loss function for the policy-value network
         Returns:
             The loss tensor
         """
-        value_loss = tf.reduce_mean(tf.square(self.yv - self.yv_))
-        #value_loss = tf.losses.mean_squared_error(labels=self.yv, predictions=self.yv_)
+        value_loss = tf.losses.mean_squared_error(labels=self.yv, predictions=self.yv_)
         policy_loss = tf.nn.softmax_cross_entropy_with_logits_v2(labels=self.yp, logits=self.yp_logits)
         self.loss = value_loss
 
     def build_conv_block(self, input_tensor, varscope):
         with tf.variable_scope(varscope, reuse=tf.AUTO_REUSE) as scope:
             Z = tf.layers.conv2d(input_tensor, filters=64, kernel_size=3, strides=1, padding="SAME")
-            Z = tf.layers.batch_normalization(Z)
+            # Z = tf.layers.batch_normalization(Z)
             A = tf.nn.relu(Z, name="A")
             return A
 
@@ -83,10 +113,11 @@ class ResNet():
 
     def build_fc_network(self, x):
         v_logits = tf.contrib.layers.flatten(x)
-        for i in range(8):
+        for i in range(4):
             v_logits = tf.contrib.layers.fully_connected(v_logits, 100)
-        v_logits = tf.contrib.layers.fully_connected(v_logits, 10)
-        v_logits = tf.contrib.layers.fully_connected(v_logits, 1)
+        # v_logits = tf.contrib.layers.fully_connected(v_logits, 10)
+        # v_logits = tf.nn.relu(v_logits)
+        v_logits = tf.contrib.layers.fully_connected(v_logits, 1, activation_fn=None)
         V = tf.nn.tanh(v_logits)
 
         return V, V, v_logits, v_logits
@@ -108,14 +139,14 @@ class ResNet():
         #Policy head
         ph1 = self.build_head_conv_layer(A, "policy_head", filter=2)
         ph1 = tf.contrib.layers.flatten(ph1)
-        p_logits = tf.contrib.layers.fully_connected(ph1, self.go_board_dimension*self.go_board_dimension+1)
+        p_logits = tf.contrib.layers.fully_connected(ph1, self.go_board_dimension*self.go_board_dimension+1, activation_fn=None)
 
         #Value head
         vh1 = self.build_head_conv_layer(A, "value_head", filter=1)
-        vh2 = tf.contrib.layers.fully_connected(vh1, 256)
-        vh2 = tf.nn.relu(vh2, name="vh2")
-        vh2 = tf.contrib.layers.flatten(vh2)
-        v_logits = tf.contrib.layers.fully_connected(vh2, 1)
+        vh2 = tf.contrib.layers.flatten(vh1)
+        vh2 = tf.contrib.layers.fully_connected(vh2, 256)
+        vh2 = tf.contrib.layers.fully_connected(vh2, 256)
+        v_logits = tf.contrib.layers.fully_connected(vh2, 1, activation_fn=None)
         
         P = tf.nn.softmax(p_logits)
         V = tf.nn.tanh(v_logits)
@@ -170,32 +201,39 @@ class ResNet():
                    train_labels_p[start_slice_index:end_slice_index],
                    train_labels_v[start_slice_index:end_slice_index])
 
-    def fake_train(self, model_path, training_data_num = 5000):
+    def fake_train(self, model_path, training_data_num = 500):
         """This function is used for testing the resNet independent of the mcts and self play code.
         The goal is to teach the resNet to count the number of black and white stones on a board.
         This code is used in test only.
         """
         batch_size = 500
-        epoch_num = 1000
+        epoch_num = 500
         fake_x, fake_yp, fake_yv = self.generate_fake_data(training_data_num)
         
         #split into batches
         for epoch in range(epoch_num):
             losses = []
+            accuracies = []
+            gradients = []
             for batch_data, batch_p, batch_v in self.generate_mini_batches(batch_size, fake_x, fake_yp, fake_yv):
-                _, batch_loss = self.sess.run(
-                        [self.train_op, self.loss],
+                _, batch_loss, batch_acc, batch_gradient = self.sess.run(
+                        [self.train_op, self.loss, self.accuracy, self.gradient],
                         feed_dict={self.x: batch_data, self.yp: batch_p, self.yv:batch_v}
                     )
                 
                 losses.append(batch_loss)
+                accuracies.append(batch_acc)
+                gradients.append(batch_gradient)
                 #print(batch_loss)
             print("Loss for epoch {} is {}".format(epoch, np.mean(losses)))
+            print("Accuracy for epoch {} is {}".format(epoch, np.mean(accuracies)))
+            #print("Gradient for epoch {} is {}".format(epoch, np.mean(gradients, axis=0)))
+            #print("Gradient for epoch {} is {}".format(epoch, gradients[0]))
             
-        #print("predicting for:" + str(fake_x[0]))
-        print("Expected labels:" + str(fake_yv[0]))
+        print("predicting for:" + str(fake_x[0:10]))
+        print("Expected labels:" + str(fake_yv[0:10]))
         print("Predicted labels:")
-        print(self.sess.run(self.yv_, feed_dict={self.x: [fake_x[0]]}))
+        print(self.sess.run([self.yv_, self.yv_logits], feed_dict={self.x: fake_x[0:10]}))
 
     def predict(self, board):
         """Given a board. predict (p,v) according to the current res net
@@ -207,7 +245,6 @@ class ResNet():
             p_dist: the probability distribution dictionary of the next move according to current policy. including pass
             v: the probability of winning from this board.
         """
-        #TODO: add current player to the input
 
         p_dist = {}
         input_to_nn = self.convert_to_resnet_input(board)
@@ -248,9 +285,9 @@ class ResNet():
             array of size 3 the element is transformed to
         """
         transformation = {
-            -1: [1,0,0],
-            0:  [0,1,0],
-            1:  [0,0,1]
+            -1: [1,0],
+            0:  [0,0],
+            1:  [0,1]
         }
         return transformation[element]
 
@@ -261,7 +298,8 @@ class ResNet():
         Returns:
             Xs: a list of training boards
             Ys: a list of training labels, each label is: 
-            [a size 26 one hot arrayindicating the count the total number stones, layer indicating current player(1) or opponent(-1) has more stones]
+            [a size 26 one hot arrayindicating the count the total number stones, layer indicating current player(1) or opponent(-1) has more stones,
+                return 1 if they have the equal number of stones]
         """
         go_board_dimension = self.go_board_dimension
         Xs = []
@@ -302,7 +340,7 @@ class ResNet():
                     player_with_more_stones = float(-1)
                 else:
                     player_with_more_stones = float(0)
-
+        
             total_stone_count_vectors.append(total_stone_count_vector)
             player_with_more_stones_all.append([float(player_with_more_stones)])
 
