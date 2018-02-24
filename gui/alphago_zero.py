dc4c9fd500500392f414709f66b864d2a2e35548
Summer Yue <yutingyue514@gmail.com>
2018-02-23 21:18:38 -0500

    experience replay in resnet training

--- gui/alphago_zero.py
+++ gui/alphago_zero.py
@@ -24,61 +24,64 @@ class AlphaGoZero():
 
     def train_nn(self, training_game_number = 1000):
         """Training the resnet by self play using MCTS
+        With experience replay
         Args:
             training_game_number: number of self play games
         Returns:
             Nothing, but model_path/game_1 has the model trained
         """
-        BATCH_SIZE = 40
+        BATCH_SIZE = 100
+        BUCKET_SIZE = 1000 # bucket size used in experience replay
         BLACK = 1 # black goes first
-        batch_training_sample_size = 0
+        batch_num = 0
+
+        # batch_training_sample_size = 0
+        bucket_training_boards = np.empty(0)
+        bucket_training_labels_p = np.empty(0)
+        bucket_training_labels_v = np.empty(0)
+
         batch_training_boards = np.empty(0)
         batch_training_labels_p = np.empty(0)
         batch_training_labels_v = np.empty(0)
 
         with self.sess.as_default():
-            for i in prog_bar(range(training_game_number)):
-                print("training game:", i+1)
+            for game_num in prog_bar(range(training_game_number)):
+                print("training game:", game_num+1)
                 board = go_board.go_board(self.nn.go_board_dimension, BLACK, board_grid=None, game_history=None)
                 ts = mcts.MCTS(board, self.nn)
                 play = self_play.self_play(board, self.nn)
-
                 training_boards, training_labels_p, training_labels_v = play.play_till_finish()
-                batch_training_sample_size += len(training_labels_v)
-                
-                if batch_training_sample_size < BATCH_SIZE:
-                    if len(batch_training_boards) == 0:
-                        batch_training_boards = training_boards
-                    else:
-                        batch_training_boards = np.append(batch_training_boards, training_boards, axis=0)
-                    if len(batch_training_labels_p) == 0:
-                        batch_training_labels_p = training_labels_p
-                    else:
-                        batch_training_labels_p = np.append(batch_training_labels_p, training_labels_p, axis=0)
-                    if len(batch_training_labels_v) == 0:
-                        batch_training_labels_v = training_labels_v
-                    else:
-                        batch_training_labels_v = np.append(batch_training_labels_v, training_labels_v, axis=0)
-                elif batch_training_sample_size > 0:
-                    if i%10 == 0:
-                        model_path = self.model_path + '/batch_' + str(i)
+            
+                # Fill the bucket with current game's boards, around 20
+                if len(bucket_training_boards) == 0:
+                    bucket_training_boards = training_boards
+                if len(bucket_training_labels_p) == 0:
+                    bucket_training_labels_p = training_labels_p
+                if len(bucket_training_labels_v) == 0:
+                    bucket_training_labels_v = training_labels_v
+                bucket_training_boards = np.append(bucket_training_boards, training_boards, axis=0)
+                bucket_training_labels_p = np.append(bucket_training_labels_p, training_labels_p, axis=0)
+                bucket_training_labels_v = np.append(bucket_training_labels_v, training_labels_v, axis=0)
+
+                # Remove from the front if bucket size exceeds the specified bucket size
+                if len(bucket_training_labels_v) > BUCKET_SIZE:
+                    deleted_indices = [i for i in range(len(bucket_training_labels_v) - BUCKET_SIZE)]
+                    bucket_training_boards = np.delete(bucket_training_boards, deleted_indices)
+                    bucket_training_labels_p = np.delete(bucket_training_labels_p, deleted_indices)
+                    bucket_training_labels_v = np.delete(bucket_training_labels_v, deleted_indices)
+
+                    # Take BATCH_SIZE number of random elements from the bucket and train
+                    BUCKET_INDICES = [i for i in range(BUCKET_SIZE)]
+                    batch_indices = np.random.choice(BUCKET_INDICES, BATCH_SIZE, replace=False)
+                    batch_training_boards = np.take(bucket_training_boards, batch_indices, axis=0)
+                    batch_training_labels_p = np.take(bucket_training_labels_p, batch_indices, axis=0)
+                    batch_training_labels_v = np.take(bucket_training_labels_v, batch_indices, axis=0)
+                    batch_num += 1
+                    if batch_num%10 == 0: #Save every 10 batches
+                        model_path = self.model_path + '/batch_' + str(batch_num)
                         self.nn.train(batch_training_boards, batch_training_labels_p, batch_training_labels_v, model_path)
                     else:
-                        self.nn.train(batch_training_boards, batch_training_labels_p, batch_training_labels_v)
-                    batch_training_boards = training_boards
-                    batch_training_labels_p = training_labels_p
-                    batch_training_labels_v = training_labels_v
-                else:
-                    if i%10 == 0:
-                        model_path = self.model_path + '/batch_' + str(i)
                         self.nn.train(batch_training_boards, batch_training_labels_p, batch_training_labels_v, model_path)
-                    else:
-                        self.nn.train(batch_training_boards, batch_training_labels_p, batch_training_labels_v)
-                    self.nn.train(training_boards, training_labels_p, training_labels_v, model_path)
-               
-            #Train the rest
-            model_path = self.model_path + '/final'
-            self.nn.train(batch_training_boards, batch_training_labels_p, batch_training_labels_v, model_path)
 
     def play_with_raw_nn(self, board):
         """Play a move with the raw res net
